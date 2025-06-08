a6de4194edf567757a96c1ec9ac9f23e0c5c8630
nikikalwar <nikikumar27@gmail.com>
2025-06-09 03:04:22 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
- // base: '/your-repo/', // Replace with your repo name
+  base: process.env.NODE_ENV === 'production' ? '/nk-dev-studio/' : '/',
 }); 
